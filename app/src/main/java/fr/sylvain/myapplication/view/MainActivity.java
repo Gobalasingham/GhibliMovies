775cbde893a2284cdbcfba21300e687f69910207
GueganVictor <KpBuWgA113?>
2019-03-28 19:39:16 +0100

    add Movie activity

--- app/src/main/java/fr/sylvain/myapplication/view/MainActivity.java
+++ app/src/main/java/fr/sylvain/myapplication/view/MainActivity.java
@@ -1,17 +1,20 @@
-package fr.sylvain.myapplication;
+package fr.sylvain.myapplication.view;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import java.util.List;
 
+import fr.sylvain.myapplication.R;
 import fr.sylvain.myapplication.controller.MainController;
 import fr.sylvain.myapplication.controller.MyAdapter;
 import fr.sylvain.myapplication.model.Movie;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private RecyclerView recyclerView;
     private RecyclerView.Adapter mAdapter;
@@ -41,8 +44,15 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
 
         // define an adapter
-        mAdapter = new MyAdapter(list);
+        mAdapter = new MyAdapter(list, this);
         recyclerView.setAdapter(mAdapter);
     }
 
+    @Override
+    public void onClick(View v) {
+        Intent intentMain = new Intent(MainActivity.this ,
+                MovieActivity.class);
+        //intentMain.putExtra("date",(String) v.getTag());
+        MainActivity.this.startActivity(intentMain);
+    }
 }
---
+++ app/src/main/java/fr/sylvain/myapplication/view/MovieActivity.java
@@ -0,0 +1,55 @@
+package fr.sylvain.myapplication.view;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+import java.util.Locale;
+
+import fr.sylvain.myapplication.R;
+import fr.sylvain.myapplication.controller.MovieController;
+import fr.sylvain.myapplication.controller.MyAdapter;
+import fr.sylvain.myapplication.model.Movie;
+
+public class MovieActivity extends AppCompatActivity {
+
+    private TextView title;
+    private TextView desc;
+    private TextView real;
+    private ImageView affiche;
+
+    private MovieController controller;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie);
+
+        title = (TextView) findViewById(R.id.title);
+        real = (TextView) findViewById(R.id.real);
+        desc = (TextView) findViewById(R.id.description);
+        affiche = (ImageView) findViewById(R.id.affiche);
+
+        controller = new MovieController(this);
+        controller.onCreate();
+        // use this setting to
+        // improve performance if you know that changes
+        // in content do not change the layout size
+        // of the RecyclerView
+    }
+
+    public void showMovie(Movie movie){
+
+        title.setText(String.format(Locale.US,"%s (%d)", movie.getTitle(), movie.getRelease_date()));
+        real.setText(new StringBuilder().append("by ").append(movie.getDirector()).toString());
+        Picasso.get().load(movie.getImage()).into(affiche);
+        desc.setText(movie.getDescription());
+    }
+
+}
